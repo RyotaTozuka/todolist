4f78be3607e905e0288a2c3be51ed2be6d087ae2
RyotaTozuka <tr.take.it.easy@gmail.com>
2019-09-12 23:28:10 +0900

    ログインページのmock作成、data追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,7 @@
 insert into user_information values (1, 'Tozuka', '$2a$10$exc.1/uNj81xqK3AdRCj/OyVRqeErJOa0iD5v3DqvJ0S6FMlDm0my');-- パスワード：pass
 insert into user_information values (2, 'Tanaka', '$2a$10$JZtpQOXNbZo5Uti3NkQ2decFigtDAzPzrTnIMTngWO6zD6mBQetae');-- パスワード：password
+
+insert into todo_list values (1,1,'〇〇を買う','20191201','0');
+insert into todo_list values (2,1,'△△の準備','20191205','0');
+insert into todo_list values (3,1,'□□をやる','20191002','1');
+insert into todo_list values (4,2,'□□に☆を返す','20191106','0');
