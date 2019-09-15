29226fe736479756be05f246d25b8081600c4396
RyotaTozuka <tr.take.it.easy@gmail.com>
2019-09-15 20:22:48 +0900

    メソッド名の修正

--- src/main/java/com/todolist/webApp/LoginController.java
+++ src/main/java/com/todolist/webApp/LoginController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
 
     @RequestMapping("login")
-    String loginHome() {
+    String login() {
         return "login";
     }
 }
