78930fda20c3c80e84d64d57dbbb14167f8cea87
RyotaTozuka <tr.take.it.easy@gmail.com>
2019-09-15 16:48:17 +0900

    ヘッダを全画面につける

--- src/main/java/com/todolist/SecurityConfig.java
+++ src/main/java/com/todolist/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/login")
+                .antMatchers("/login","/header/header")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
